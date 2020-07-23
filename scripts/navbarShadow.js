3cef46fcb4204c808efab043cfd7f3ce6e48c45c
hasin-shabbir <66528929+hasin-shabbir@users.noreply.github.com>
2020-07-24 02:32:41 +0500

    Landing page created

--- scripts/navbarShadow.js
+++ scripts/navbarShadow.js
@@ -1,13 +1,9 @@
 /* Add shadow to the bottom of navbar on scroll and remove when page is scrolled to the top */
 window.onscroll = function(){addShadow()};
 function addShadow(){
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById("topnav").classList.add('navbar-shadow');
-    document.getElementById("topnav").style.paddingTop="0px";
-    document.getElementById("topnav").style.paddingBottom="0px";
   } else {
     document.getElementById("topnav").classList.remove('navbar-shadow');
-    document.getElementById("topnav").style.paddingTop="0.5rem";
-    document.getElementById("topnav").style.paddingBottom="0.5rem";
   }
 }
---
+++ scripts/navbarStyling.js
@@ -0,0 +1,22 @@
+var lastScrollTop = window.pageYOffset;
+window.addEventListener("scroll", function(){
+  if(window.innerWidth>998){
+     var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+     if (st > lastScrollTop){
+        // downscroll code
+        document.getElementById("topnav").style.marginTop="-90px";
+      //  document.getElementById("topnav").style.display="none";
+     } else {
+        // upscroll code
+        document.getElementById("topnav").style.marginTop="initial";
+        if (window.pageYOffset!==0) {
+          document.getElementById("topnav").style.backgroundColor="white";
+        }
+        else{
+          document.getElementById("topnav").style.backgroundColor="transparent";
+        }
+  //document.getElementById("topnav").style.display="initial";
+     }
+     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+   }
+}, false);
