54c4019de2a432cc9bf0c46793dc118616b40110
hasin-shabbir <66528929+hasin-shabbir@users.noreply.github.com>
2020-07-25 00:55:18 +0500

    navbar functionality improvement

--- scripts/navbarStyling.js
+++ scripts/navbarStyling.js
@@ -1,6 +1,6 @@
 var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
-  if(window.innerWidth>992){
+  //if(window.innerWidth>992){
      var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
      if (st > lastScrollTop){
         // downscroll code
@@ -10,7 +10,12 @@ window.addEventListener("scroll", function(){
         // upscroll code
         document.getElementById("topnav").style.marginTop="initial";
         if (window.pageYOffset!==0) {
-          document.getElementById("topnav").style.backgroundColor="white";
+          if(window.innerWidth>992){
+            document.getElementById("topnav").style.backgroundColor="white";
+          }
+          else {
+            document.getElementById("topnav").style.backgroundColor="#e6e6e6";
+          }
         }
         else{
           document.getElementById("topnav").style.backgroundColor="transparent";
@@ -18,5 +23,5 @@ window.addEventListener("scroll", function(){
   //document.getElementById("topnav").style.display="initial";
      }
      lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-   }
+  // }
 }, false);
