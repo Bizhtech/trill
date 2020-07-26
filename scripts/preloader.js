161bfad5797543b6eff15fb49bdc55372e1bf4e1
hasin-shabbir <66528929+hasin-shabbir@users.noreply.github.com>
2020-07-27 01:01:11 +0500

    syling changes

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-  document.getElementById('preloader').style.display="none";
+  document.getElementById('preloader').classList.add('fadeItOut');
+  setTimeout(function(){
+    document.getElementById('preloader').style.display="none";
+  },2000);
+
 })
